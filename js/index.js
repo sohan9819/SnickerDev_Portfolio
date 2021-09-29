2dfaca684940db1571aadab5ccb62411136bfbee
sohan9819 <sohanshetty2001@gmail.com>
2021-09-29 11:26:33 +0530

    Added diff page for blog and project

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
 // const navSlide = () => {
-//     const burger = document.querySelector('.burger');
-//     const nav = document.querySelector('.nav-links');
-//     const navLinks = document.querySelectorAll('.nav-links li');
+//   const burger = document.querySelector(".burger");
+//   const nav = document.querySelector(".nav-links");
+//   const navLinks = document.querySelectorAll(".nav-links li");
 
-//     burger.addEventListener('click', () => {
-//         //Toggle Lav
-//         nav.classList.toggle('nav-active');
-//         //Animate Links
-//         navLinks.forEach((link, index) => {
-//             if (link.style.animation) {
-//                 link.style.animation = '';
-//             } else {
-//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-//             }
-//         })
-//         //Burger Animation
-//         burger.classList.toggle('toggle');
-//     })
-// }
+//   burger.addEventListener("click", () => {
+//     //Toggle Lav
+//     nav.classList.toggle("nav-active");
+//     //Animate Links
+//     navLinks.forEach((link, index) => {
+//       if (link.style.animation) {
+//         link.style.animation = "";
+//       } else {
+//         link.style.animation = `navLinkFade 0.5s ease forwards ${
+//           index / 7 + 0.3
+//         }s`;
+//       }
+//     });
+//     //Burger Animation
+//     burger.classList.toggle("toggle");
+//   });
+// };
 
 // navSlide();
