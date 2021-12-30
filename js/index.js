c76bd5a700e722094f365a2433b2ba5c82116609
sohan9819 <sohanshetty2001@gmail.com>
2021-12-30 13:35:17 +0530

    removed all console logs and added snicker logo

--- js/index.js
+++ js/index.js
@@ -2,17 +2,15 @@ const navbar_burger = document.querySelector('.burger');
 const navbar = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav-links > li');
 
-console.log(navLinks);
-
 const toggleActive = (e) => {
-  navbar.classList.toggle('active');
-  navbar_burger.classList.toggle('active');
+  if (window.screen.width <= 800) {
+    navbar.classList.toggle('active');
+    navbar_burger.classList.toggle('active');
+  }
 };
 navLinks.forEach((links) => {
   links.addEventListener('click', toggleActive);
 });
-
-// console.log(navbar, navbar_burger);
 
 navbar_burger.addEventListener('click', toggleActive);
 
@@ -20,7 +18,6 @@ navbar_burger.addEventListener('click', toggleActive);
 
 const loader = document.querySelector('#preloader');
 const sections = document.querySelectorAll('section');
-// console.log(loader);
 
 sections.forEach((section) => {
   section.style.display = 'none';
@@ -39,28 +36,6 @@ window.addEventListener('load', () => {
 });
 
 // #######################################
-
-// const links = document.querySelectorAll('a');
-// console.log(links);
-// links.forEach((link) => {
-//   link.addEventListener('click', removeHash);
-// });
-// // console.log(window.location.origin);
-// // console.log(window.location.pathname);
-// // console.log(window.location.search);
-
-// console.log(location.href);
-// function removeHash(e) {
-//   // console.log('hello world');
-//   // history.replaceState(
-//   //   '',
-//   //   document.title,
-//   //   window.location.origin + window.location.pathname
-//   // );
-//   location.href = location.href.replace(location.hash, '');
-//   e.preventDefault();
-// }
-
 document.querySelectorAll('a').forEach(function (current) {
   if (!current.hash) return;
   if (current.origin + current.pathname != self.location.href) return;
